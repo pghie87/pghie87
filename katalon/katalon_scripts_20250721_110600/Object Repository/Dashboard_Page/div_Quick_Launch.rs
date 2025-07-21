69ad2c28b7deaeec8d99fe216f7fef54bbd94e26
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:09:00 +0700

    Upload div_Quick_Launch.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/div_Quick_Launch.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Quick Launch widget on dashboard</description>
+   <name>div_Quick_Launch</name>
+   <tag></tag>
+   <elementGuidId>9i0j1k2l-3m4n-5o6p-7q8r-9s0t1u2v3w4x</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(.,'Quick Launch')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.orangehrm-dashboard-widget:nth-child(3)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(.,'Quick Launch') and contains(@class,'orangehrm-dashboard-widget')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
