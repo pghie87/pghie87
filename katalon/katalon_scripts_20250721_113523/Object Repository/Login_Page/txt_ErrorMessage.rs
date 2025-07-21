fb8da344e5f31080d65e4c7bea3c694c2a7551f6
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:38:05 +0700

    Upload txt_ErrorMessage.rs

---
+++ katalon/katalon_scripts_20250721_113523/Object Repository/Login_Page/txt_ErrorMessage.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Error message displayed on invalid login</description>
+   <name>txt_ErrorMessage</name>
+   <tag></tag>
+   <elementGuidId>456789ab-defg-hijk-lmno-456789012def</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(@class, 'alert-content') or contains(@class, 'oxd-alert-content')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-alert-content-text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'alert-content-text')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>aaaaaaaa-bbbb-cccc-dddd-111111111111</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
