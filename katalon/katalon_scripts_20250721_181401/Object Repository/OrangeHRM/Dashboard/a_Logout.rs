1be00665a10e08883722ad95db7bacd6597a8485
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 18:17:14 +0700

    Upload a_Logout.rs

---
+++ katalon/katalon_scripts_20250721_181401/Object Repository/OrangeHRM/Dashboard/a_Logout.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Logout link in dropdown menu</description>
+   <name>a_Logout</name>
+   <tag></tag>
+   <elementGuidId>i9j0k1l2-m3n4-o5p6-q7r8-s9t0u1v2w3x4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[text()='Logout']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(), 'Logout')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>ul.oxd-dropdown-menu > li:last-child > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>o5p6q7r8-s9t0-u1v2-w3x4-y5z6a7b8c9d0</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
