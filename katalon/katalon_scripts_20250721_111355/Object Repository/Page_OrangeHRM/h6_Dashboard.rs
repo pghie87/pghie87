d3d89a3f8f611c725b9efd1febf2be167f8bada3
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:16:03 +0700

    Upload h6_Dashboard.rs

---
+++ katalon/katalon_scripts_20250721_111355/Object Repository/Page_OrangeHRM/h6_Dashboard.rs
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Dashboard title</description>
+   <name>h6_Dashboard</name>
+   <tag></tag>
+   <elementGuidId>i6j7k8l9-m0n1-o2p3-q4r5-s6t7u8v9w0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h6[text()='Dashboard']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(@class, 'oxd-text') and text()='Dashboard']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>.oxd-topbar-header-breadcrumb > h6</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>l8m9n0o1-p2q3-r4s5-t6u7-v8w9x0y1z2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>oxd-text</value>
+      <webElementGuid>m9n0o1p2-q3r4-s5t6-u7v8-w9x0y1z2a3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Dashboard</value>
+      <webElementGuid>n0o1p2q3-r4s5-t6u7-v8w9-x0y1z2a3b4</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
