20c4596580a87627ecb01ec6d990e38e0d9f529e
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:08:41 +0700

    Upload input_Password.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Login_Page/input_Password.rs
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Password input field on login page</description>
+   <name>input_Password</name>
+   <tag></tag>
+   <elementGuidId>2b3c4d5e-6f7g-8h9i-0j1k-2l3m4n5o6p7q</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//input[@name='password']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>input[name='password']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+      <webElementGuid>eeee1111-2222-3333-4444-555566667777</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>password</value>
+      <webElementGuid>aaaa1111-bbbb-cccc-dddd-eeeeffffgggg</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>password</value>
+      <webElementGuid>hhhh1111-iiii-jjjj-kkkk-llllmmmmnnnn</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
