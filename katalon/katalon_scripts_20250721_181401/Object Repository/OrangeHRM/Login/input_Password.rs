4220db74c04fc4020300611bd41acf9b627f9384
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 18:17:15 +0700

    Upload input_Password.rs

---
+++ katalon/katalon_scripts_20250721_181401/Object Repository/OrangeHRM/Login/input_Password.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Password input field on login page</description>
+   <name>input_Password</name>
+   <tag></tag>
+   <elementGuidId>b2c3d4e5-f6g7-8h90-i1j2-k3l4m5n67890</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//input[@name='password' or @type='password']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='password' or @type='password']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>input[name='password'], input[type='password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+      <webElementGuid>d4e5f6g7-h8i9-j0k1-l2m3-n4o5p6q7r8s9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>password</value>
+      <webElementGuid>e5f6g7h8-i9j0-k1l2-m3n4-o5p6q7r8s9t0</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
