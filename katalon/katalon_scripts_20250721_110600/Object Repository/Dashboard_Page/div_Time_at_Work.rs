24ab4a6430947029cff02ab562381d36720791e5
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:08:47 +0700

    Upload div_Time_at_Work.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/div_Time_at_Work.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Time at Work widget on dashboard</description>
+   <name>div_Time_at_Work</name>
+   <tag></tag>
+   <elementGuidId>7g8h9i0j-1k2l-3m4n-5o6p-7q8r9s0t1u2v</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(.,'Time at Work')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.orangehrm-dashboard-widget:nth-child(1)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(.,'Time at Work') and contains(@class,'orangehrm-dashboard-widget')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
