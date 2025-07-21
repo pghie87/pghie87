c930e3c98021f2c323f4ff38e632df18e18f7422
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:09:04 +0700

    Upload div_My_Actions.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/div_My_Actions.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>My Actions widget on dashboard</description>
+   <name>div_My_Actions</name>
+   <tag></tag>
+   <elementGuidId>8h9i0j1k-2l3m-4n5o-6p7q-8r9s0t1u2v3w</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(.,'My Actions')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.orangehrm-dashboard-widget:nth-child(2)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(.,'My Actions') and contains(@class,'orangehrm-dashboard-widget')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
