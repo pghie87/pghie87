2d272b1ca30974f99343ef0d62a3efa6319d2ca0
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:08:49 +0700

    Upload a_Admin.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/a_Admin.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Admin menu item</description>
+   <name>a_Admin</name>
+   <tag></tag>
+   <elementGuidId>1k2l3m4n-5o6p-7q8r-9s0t-1u2v3w4x5y6z</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[contains(.,'Admin')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.oxd-main-menu-item:nth-child(1)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='Admin']/parent::a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
