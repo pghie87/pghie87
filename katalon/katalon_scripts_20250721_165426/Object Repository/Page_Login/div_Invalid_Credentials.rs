632fdcfab2ce2f0941905f42bb6dd391d64c44c7
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 16:56:37 +0700

    Upload div_Invalid_Credentials.rs

---
+++ katalon/katalon_scripts_20250721_165426/Object Repository/Page_Login/div_Invalid_Credentials.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Invalid credentials error message</description>
+   <name>div_Invalid_Credentials</name>
+   <tag></tag>
+   <elementGuidId>e1f2g3h4-i5j6-k7l8-m9n0-o1p2q3r4s5t6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(@class, 'alert') and contains(@class, 'danger')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'oxd-alert-content--error')]/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-alert-content--error p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>12345678-9abc-def0-1234-56789abcdef0</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
