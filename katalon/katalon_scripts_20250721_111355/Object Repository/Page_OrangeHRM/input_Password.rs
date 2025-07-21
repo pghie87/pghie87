540ca730d330f0f108fe7802dd91135c592433ce
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:16:08 +0700

    Upload input_Password.rs

---
+++ katalon/katalon_scripts_20250721_111355/Object Repository/Page_OrangeHRM/input_Password.rs
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Password input field</description>
+   <name>input_Password</name>
+   <tag></tag>
+   <elementGuidId>f4g5h6i7-j8k9-l0m1-n2o3-p4q5r6s7t8u</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//input[@name='password']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='password']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>input[name='password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+      <webElementGuid>g5h6i7j8-k9l0-m1n2-o3p4-q5r6s7t8u9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>password</value>
+      <webElementGuid>h6i7j8k9-l0m1-n2o3-p4q5-r6s7t8u9v0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>Password</value>
+      <webElementGuid>i7j8k9l0-m1n2-o3p4-q5r6-s7t8u9v0w1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>password</value>
+      <webElementGuid>j8k9l0m1-n2o3-p4q5-r6s7-t8u9v0w1x2</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
