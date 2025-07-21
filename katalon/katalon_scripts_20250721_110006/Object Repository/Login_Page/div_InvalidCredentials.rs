8bb0c78bf56476e20d5fdbb57b4a39b4aadbf0a1
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:02:49 +0700

    Upload div_InvalidCredentials.rs

---
+++ katalon/katalon_scripts_20250721_110006/Object Repository/Login_Page/div_InvalidCredentials.rs
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Invalid credentials error message</description>
+   <name>div_InvalidCredentials</name>
+   <tag></tag>
+   <elementGuidId>78901234-ghij-7890-klmn-78901234abcd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(@class, 'alert-content-text') and contains(text(), 'Invalid credentials')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-alert-content-text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'alert-content-text') and contains(text(), 'Invalid credentials')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>12345678-1234-5678-1234-567890abcdef</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>alert-content-text</value>
+      <webElementGuid>23456789-2345-6789-2345-6789abcdefgh</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Invalid credentials</value>
+      <webElementGuid>34567890-3456-7890-3456-7890abcdefgh</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
