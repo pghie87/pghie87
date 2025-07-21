c14c57c0cb8f5b1c8c63eccbfee29dc4d65f06ad
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 18:17:21 +0700

    Upload div_InvalidCredentials.rs

---
+++ katalon/katalon_scripts_20250721_181401/Object Repository/OrangeHRM/Login/div_InvalidCredentials.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Invalid credentials error message</description>
+   <name>div_InvalidCredentials</name>
+   <tag></tag>
+   <elementGuidId>e5f6g7h8-i9j0-k1l2-m3n4-o5p6q7r8s9t0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(@class, 'alert-content') or contains(@class, 'oxd-alert-content')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'alert-content')]/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-alert-content > p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>j0k1l2m3-n4o5-p6q7-r8s9-t0u1v2w3x4y5</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
