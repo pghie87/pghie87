319618375c1a5a55fae2477578d5df1344b9662d
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:38:12 +0700

    Upload link_ForgotPassword.rs

---
+++ katalon/katalon_scripts_20250721_113523/Object Repository/Login_Page/link_ForgotPassword.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Forgot password link</description>
+   <name>link_ForgotPassword</name>
+   <tag></tag>
+   <elementGuidId>56789abc-efgh-ijkl-mnop-56789012efgh</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//p[contains(.,'Forgot your password?')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p.oxd-text.oxd-text--p.orangehrm-login-forgot-header</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[contains(text(),'Forgot your password?')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>bbbbbbbb-cccc-dddd-eeee-222222222222</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
