29a5c795960d425bf8e64c654d67eb9c4d6ba003
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 18:17:23 +0700

    Upload p_ForgotPassword.rs

---
+++ katalon/katalon_scripts_20250721_181401/Object Repository/OrangeHRM/Login/p_ForgotPassword.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Forgot password link</description>
+   <name>p_ForgotPassword</name>
+   <tag></tag>
+   <elementGuidId>f6g7h8i9-j0k1-l2m3-n4o5-p6q7r8s9t0u1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//p[contains(text(), 'Forgot your password?')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[contains(text(), 'Forgot your password?')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p.oxd-text.oxd-text--p.orangehrm-login-forgot-header</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>k1l2m3n4-o5p6-q7r8-s9t0-u1v2w3x4y5z6</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
