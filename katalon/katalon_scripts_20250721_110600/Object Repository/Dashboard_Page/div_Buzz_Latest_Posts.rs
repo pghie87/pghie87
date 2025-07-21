501a3ccb8560f49a35345869eacd690ef1a55235
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:09:02 +0700

    Upload div_Buzz_Latest_Posts.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/div_Buzz_Latest_Posts.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Buzz Latest Posts widget on dashboard</description>
+   <name>div_Buzz_Latest_Posts</name>
+   <tag></tag>
+   <elementGuidId>0j1k2l3m-4n5o-6p7q-8r9s-0t1u2v3w4x5y</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(.,'Buzz Latest Posts')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.orangehrm-dashboard-widget:nth-child(4)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(.,'Buzz Latest Posts') and contains(@class,'orangehrm-dashboard-widget')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
