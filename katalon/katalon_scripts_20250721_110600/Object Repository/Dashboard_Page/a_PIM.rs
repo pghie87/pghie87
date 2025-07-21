bab2b43f45d7da7a7c8e1a7a05013be5f680cb77
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:08:57 +0700

    Upload a_PIM.rs

---
+++ katalon/katalon_scripts_20250721_110600/Object Repository/Dashboard_Page/a_PIM.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>PIM menu item</description>
+   <name>a_PIM</name>
+   <tag></tag>
+   <elementGuidId>2l3m4n5o-6p7q-8r9s-0t1u-2v3w4x5y6z7a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[contains(.,'PIM')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.oxd-main-menu-item:nth-child(2)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='PIM']/parent::a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
