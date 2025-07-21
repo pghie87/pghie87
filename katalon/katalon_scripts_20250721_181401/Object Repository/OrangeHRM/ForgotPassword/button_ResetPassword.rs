057a11bf8db22ca7e3603f0fcde68f4a1ce013e5
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 18:17:09 +0700

    Upload button_ResetPassword.rs

---
+++ katalon/katalon_scripts_20250721_181401/Object Repository/OrangeHRM/ForgotPassword/button_ResetPassword.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Reset Password button</description>
+   <name>button_ResetPassword</name>
+   <tag></tag>
+   <elementGuidId>l2m3n4o5-p6q7-r8s9-t0u1-v2w3x4y5z6a7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@type='submit']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(text(), 'Reset Password')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-forgot-password-button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>s9t0u1v2-w3x4-y5z6-a7b8-c9d0e1f2g3h4</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
