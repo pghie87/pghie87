1170a93b6762a0d30d0764599f84aa7247b61558
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 11:37:59 +0700

    Upload header_ResetPassword.rs

---
+++ katalon/katalon_scripts_20250721_113523/Object Repository/ForgotPassword_Page/header_ResetPassword.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Reset Password page header</description>
+   <name>header_ResetPassword</name>
+   <tag></tag>
+   <elementGuidId>abcdefgh-ijkl-mnop-qrst-012klmnopqrs</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h6[contains(text(),'Reset Password')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h6.oxd-text.oxd-text--h6.orangehrm-forgot-password-title</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(text(),'Reset Password')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>kkkkkkkk-llll-mmmm-nnnn-bbbbbbbbbbbb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Reset Password</value>
+      <webElementGuid>llllllll-mmmm-nnnn-oooo-cccccccccccc</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
