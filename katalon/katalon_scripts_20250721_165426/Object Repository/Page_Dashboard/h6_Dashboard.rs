4362b6a43a229c924a822d1a4019898d898311fc
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-21 16:56:41 +0700

    Upload h6_Dashboard.rs

---
+++ katalon/katalon_scripts_20250721_165426/Object Repository/Page_Dashboard/h6_Dashboard.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Dashboard heading</description>
+   <name>h6_Dashboard</name>
+   <tag></tag>
+   <elementGuidId>f1g2h3i4-j5k6-l7m8-n9o0-p1q2r3s4t5u6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h6[contains(text(), 'Dashboard')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(text(), 'Dashboard')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h6.oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>fedcba98-7654-3210-fedc-ba9876543210</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
