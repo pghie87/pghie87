1740cc93adff8cc8d5a597cc976f4cdcb9d86133
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-23 16:52:39 +0700

    Upload h6_ResetPassword.rs

---
+++ katalon/katalon_scripts_20250723_095021/Object Repository/Page_OrangeHRM_ForgotPassword/h6_ResetPassword.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Reset Password header text</description>
+   <name>h6_ResetPassword</name>
+   <tag></tag>
+   <elementGuidId>i9012345-0123-4567-89ab-cdef01234567</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h6[contains(text(), 'Reset Password')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(text(), 'Reset Password')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h6.oxd-text.oxd-text--h6.orangehrm-forgot-password-title</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>i9012345-0123-4567-89ab-cdef01234567</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Reset Password</value>
+      <webElementGuid>j0123456-1234-5678-9abc-def012345678</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
