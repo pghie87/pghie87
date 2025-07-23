b71f8009ba7673c4054ca555ddb34af516438507
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-23 16:52:46 +0700

    Upload link_Logout.rs

---
+++ katalon/katalon_scripts_20250723_095021/Object Repository/Page_OrangeHRM_Common/link_Logout.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Logout link in the user dropdown menu</description>
+   <name>link_Logout</name>
+   <tag></tag>
+   <elementGuidId>h8901234-f012-3456-789a-bcdef0123456</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[contains(text(), 'Logout')]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(), 'Logout')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.oxd-userdropdown-link:contains('Logout')</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>h8901234-f012-3456-789a-bcdef0123456</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Logout</value>
+      <webElementGuid>i9012345-0123-4567-89ab-cdef01234567</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
