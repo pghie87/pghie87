1ad5440f9b16306d5203f16d5e3b24acfb2e5f85
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-07-23 16:52:55 +0700

    Upload div_ErrorMessage.rs

---
+++ katalon/katalon_scripts_20250723_095021/Object Repository/Page_OrangeHRM_Login/div_ErrorMessage.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Error message displayed after invalid login attempt</description>
+   <name>div_ErrorMessage</name>
+   <tag></tag>
+   <elementGuidId>d4567890-bcde-f012-3456-789abcdef012</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='oxd-alert-content oxd-alert-content--error']/p</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='oxd-alert-content oxd-alert-content--error']/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-alert-content.oxd-alert-content--error > p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>d4567890-bcde-f012-3456-789abcdef012</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
