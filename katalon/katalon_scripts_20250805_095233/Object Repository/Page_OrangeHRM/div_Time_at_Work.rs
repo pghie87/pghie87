66d93a1166f259d4baaf8fc58dc74eeb7e49ce59
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-08-05 09:54:44 +0700

    Upload div_Time_at_Work.rs

---
+++ katalon/katalon_scripts_20250805_095233/Object Repository/Page_OrangeHRM/div_Time_at_Work.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Time at Work section on Dashboard page</description>
+   <name>div_Time_at_Work</name>
+   <tag></tag>
+   <elementGuidId>h8i9j0k1-l2m3-4567-hijk-l8m9n0o1p2q3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[.//p[text()='Time at Work']]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.oxd-sheet:has(p:contains('Time at Work'))</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'oxd-sheet') and .//p[contains(text(), 'Time at Work')]]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>t0u1v2w3-4567-89st-uvwx-z0a1b2c3d4e5</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
