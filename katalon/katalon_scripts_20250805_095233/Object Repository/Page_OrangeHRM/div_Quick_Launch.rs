2c6cca97f82ce0f593fb9ebd4e51a81123151129
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-08-05 09:54:58 +0700

    Upload div_Quick_Launch.rs

---
+++ katalon/katalon_scripts_20250805_095233/Object Repository/Page_OrangeHRM/div_Quick_Launch.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Quick Launch section on Dashboard page</description>
+   <name>div_Quick_Launch</name>
+   <tag></tag>
+   <elementGuidId>g7h8i9j0-k1l2-3456-ghij-k7l8m9n0o1p2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[contains(@class, 'orangehrm-quick-launch')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.orangehrm-quick-launch</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'orangehrm-quick-launch')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>r8s9t0u1-2345-67qr-stuv-x8y9z0a1b2c3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>orangehrm-quick-launch</value>
+      <webElementGuid>s9t0u1v2-3456-78rs-tuvw-y9z0a1b2c3d4</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
